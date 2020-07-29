f018c1c72494ecdea7b362945bd5d94bd89451bf
lihangfei <2267382583@qq.com>
2020-07-29 16:30:56 +0800

    最后了aaaa123456

--- src/main/java/com/systop/sjz_travel/Mapper/pictureMapper.java
+++ src/main/java/com/systop/sjz_travel/Mapper/pictureMapper.java
@@ -3,5 +3,12 @@ package com.systop.sjz_travel.Mapper;
 import org.springframework.stereotype.Service;
 
 public interface pictureMapper {
-
+//sadasdasd
+//    ad
+//    asd
+//        a
+//    sd
+//                as
+//    d
+//                        asd
 }
